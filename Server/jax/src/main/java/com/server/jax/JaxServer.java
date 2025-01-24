fd3bc722a0b6190f97811861a59f9d09655b60ac
Bogdan Petrovic <bogdanxpetrovic@gmail.com>
2025-01-24 16:25:14 +0100

    remove spring annotation, add endpoint publish

--- Server/jax/src/main/java/com/server/jax/JaxServer.java
+++ Server/jax/src/main/java/com/server/jax/JaxServer.java
@@ -1,13 +1,14 @@
 package com.server.jax;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.server.jax.implementations.CalculatorImpl;
+import com.server.jax.services.CalculatorService;
 
-@SpringBootApplication
-public class JaxApplication {
+import jakarta.xml.ws.Endpoint;
 
+public class JaxServer {
 	public static void main(String[] args) {
-		SpringApplication.run(JaxApplication.class, args);
+		String url = "http://localhost:8080/calculator";
+		Endpoint.publish(url, new CalculatorImpl());
 	}
 
 }
